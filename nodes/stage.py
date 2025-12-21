991f4de9a5b2603073075330cd687e1853eba574
Minh Quan Do <mdo6180@gmail.com>
2025-12-21 16:09:59 -0500

    first commit

---
+++ nodes/stage.py
@@ -0,0 +1,37 @@
+import threading
+from queue import Queue
+from typing import Dict
+import time
+
+
+
+class BaseStageNode(threading.Thread):
+    def __init__(self, name: str):
+        self.predecessor_queues: Dict[str, Queue] = {}
+        self.successor_queues: Dict[str, Queue] = {}
+        self.exit_event = threading.Event()
+
+        super().__init__(name=name)
+    
+    def wait_predecessors(self):
+        # Wait until all predecessor queues have at least one item
+        while any(q.empty() for q in self.predecessor_queues.values()):
+            time.sleep(0.1)  # Avoid busy waiting
+
+        for signal_name, queue in self.predecessor_queues.items():
+            signal = queue.get()
+            print(f"{self.name} consumed signal: {signal_name} with value {signal}")
+    
+    def signal_successors(self):
+        for signal_name, queue in self.successor_queues.items():
+            queue.put(f"Signal from {self.name}")
+            print(f"{self.name} produced signal: {signal_name}")
+    
+    def execute(self):
+        print(f"{self.name} executing")
+    
+    def run(self):
+        while not self.exit_event.is_set():
+            self.wait_predecessors()
+            self.execute()
+            self.signal_successors()
---
+++ nodes/watcher.py
@@ -0,0 +1,63 @@
+import threading
+from queue import Queue
+from typing import Dict
+from abc import ABC, abstractmethod
+
+
+
+class BaseWatcherNode(threading.Thread, ABC):
+    def __init__(self, name: str):
+        self.successor_queues: Dict[str, Queue] = {}
+        self.exit_event = threading.Event()
+        self.resource_event = threading.Event()
+        super().__init__(name=name)
+    
+    def exit(self):
+        self.stop_monitoring()
+        self.resource_event.set()
+    
+    @abstractmethod
+    def start_monitoring(self) -> None:
+        """
+        Override to specify how the resource is monitored. 
+        Typically, this method will be used to start an observer that runs in a child thread spawned by the thread running the node.
+        """
+        pass
+    
+    @abstractmethod
+    def stop_monitoring(self) -> None:
+        """
+        Override to specify how the resource is monitored. 
+        Typically, this method will be used to start an observer that runs in a child thread spawned by the thread running the node.
+        """
+        pass
+
+    def trigger(self, message: str = None) -> None:
+        if self.resource_event.is_set() is False:
+            self.resource_event.set()
+            print(f"{self.name} triggered with message: {message}")
+    
+    def signal_successors(self):
+        for signal_name, queue in self.successor_queues.items():
+            queue.put(f"Signal from {self.name}")
+            print(f"{self.name} produced signal: {signal_name}")
+    
+    def execute(self):
+        print(f"{self.name} executing")
+    
+    def run(self):
+        self.start_monitoring()     # Start monitoring the resource in a separate thread
+
+        while not self.exit_event.is_set():
+
+            if self.exit_event.is_set(): return
+            self.resource_event.wait()      # Wait until the resource event is set
+
+            if self.exit_event.is_set(): return
+            self.execute()
+
+            if self.exit_event.is_set(): return
+            self.signal_successors()
+
+            if self.exit_event.is_set(): return
+            self.resource_event.clear()     # Reset the event for the next cycle
