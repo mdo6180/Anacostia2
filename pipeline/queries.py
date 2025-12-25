24a0d6e1749d2e46d1041612e28ad23e5d3bec31
Minh Quan Do <mdo6180@gmail.com>
2025-12-25 02:25:00 -0500

    removed queries file

--- pipeline/queries.py
+++
@@ -1,8 +0,0 @@
-create_events_table = """
-CREATE TABLE IF NOT EXISTS events (
-    event_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    node_id INTEGER,
-    event_type TEXT,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-"""
