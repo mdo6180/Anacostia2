cb228e838b2e788c4bbf7295c5618894ae15fb1d
Minh Quan Do <mdo6180@gmail.com>
2026-02-02 18:05:33 -0500

    added example of using iterators

---
+++ stream.py
@@ -0,0 +1,113 @@
+import os
+import time
+import threading
+import queue
+
+
+
+input_path1 = "./testing_artifacts/incoming1"
+input_path2 = "./testing_artifacts/incoming2"
+
+
+class DirectoryStream:
+    def __init__(self, directory: str, poll_interval: float = 1.0, batch_size: int = 1):
+        if os.path.exists(directory) is False:
+            print(f"Directory {directory} does not exist. Creating it.")
+            os.makedirs(directory)
+
+        if batch_size <= 0:
+            raise ValueError("batch_size must be >= 1")
+
+        self.directory = directory
+        self.poll_interval = poll_interval
+        self.batch_size = batch_size
+        self.seen = set()
+
+    def poll(self):
+        batch_paths = []
+        batch_content = []
+
+        while True:
+            for filename in sorted(os.listdir(self.directory)):
+                if filename in self.seen:
+                    continue
+
+                path = os.path.join(self.directory, filename)
+                if not os.path.isfile(path):
+                    continue
+
+                self.seen.add(filename)
+                batch_paths.append(path)
+
+                # Read file content
+                with open(path, 'r') as file:
+                    content = file.read()
+                    batch_content.append(content)
+
+                # Only yield when the batch is FULL
+                if len(batch_paths) >= self.batch_size:
+                    yield batch_paths, batch_content
+                    batch_paths = []
+                    batch_content = []
+
+            # IMPORTANT: do NOT yield partial batches here.
+            time.sleep(self.poll_interval)
+
+
+class StreamRunner:
+    def __init__(self, stream: DirectoryStream, maxsize=0):
+        self.stream = stream
+        self.items_queue = queue.Queue(maxsize=maxsize)
+        self._stop = threading.Event()
+        self._thread = None
+
+    def start(self):
+        def run():
+            for item in self.stream.poll():
+                if self._stop.is_set():
+                    break
+                self.items_queue.put(item=item, block=True)  # backpressure here, blocks if queue is full
+        self._thread = threading.Thread(target=run, daemon=True)
+        self._thread.start()
+        return self
+
+    def stop(self):
+        self._stop.set()
+
+    def __iter__(self):
+        while not self._stop.is_set():
+            batch_paths, batch_content = self.items_queue.get(block=True)  # blocks until item is available
+            print(f"StreamRunner registering paths to DB: {batch_paths}")
+            yield batch_content
+
+
+if __name__ == "__main__":
+    try:
+        """
+        # Test 0: Single DirectoryStream with batch_size=1
+        runner1 = StreamRunner(DirectoryStream(input_path1)).start()
+        for batch1 in runner1:
+            print(f"New file detected: {batch1}")
+
+        # Test 1: Single DirectoryStream with batch_size=2
+        runner1 = StreamRunner(DirectoryStream(input_path1, batch_size=2)).start()
+        for batch1 in runner1:
+            print(f"New file detected: {batch1}")
+        
+        # Test 2: Two DirectoryStreams with batch_size=2
+        runner1 = StreamRunner(DirectoryStream(input_path1, batch_size=2)).start()
+        runner2 = StreamRunner(DirectoryStream(input_path2, batch_size=2)).start()
+        for batch1, batch2 in zip(runner1, runner2):
+            print(f"New file detected: {batch1}, {batch2}")
+
+        """
+        runner1 = StreamRunner(DirectoryStream(input_path1, batch_size=2)).start()
+        runner2 = StreamRunner(DirectoryStream(input_path2, batch_size=2)).start()
+
+        for batch1, batch2 in zip(runner1, runner2):
+            print(f"New file detected: {batch1}, {batch2}")
+
+    except KeyboardInterrupt:
+        print("Stopping stream runners...")
+        runner1.stop()
+        runner2.stop()
---
+++ stream_test.py
@@ -0,0 +1,22 @@
+import time
+
+
+def create_file(file_path, content):
+    try:
+        with open(file_path, 'w') as file:
+            file.write(content)
+        print(f"File '{file_path}' created successfully.")
+    except Exception as e:
+        print(f"Error creating the file: {e}")
+
+
+if __name__ == "__main__":
+    tests_path = f"./testing_artifacts"
+    data_store1_input = f"{tests_path}/incoming1"
+    data_store2_input = f"{tests_path}/incoming2"
+
+    for i in range(10):
+        create_file(f"{data_store1_input}/test_file{i}.txt", f"incoming1 {i}")
+        time.sleep(1.5)
+        create_file(f"{data_store2_input}/test_file{i}.txt", f"incoming2 {i}")
+        time.sleep(1.5)
