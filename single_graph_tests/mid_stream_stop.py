cecdc7e2ec12ce068b6a4f56a702b57fb9bcc0fc
Minh Quan Do <mdo6180@gmail.com>
2026-06-05 17:22:26 -0400

    renamed a file

--- single_graph_tests/mid_stream_stop.py
+++ single_graph_tests/mid_stream_stop.py
@@ -3,38 +3,45 @@ import os
 import argparse
 import shutil
 import time
+from pathlib import Path
 
-from streams.directory import DirectoryStream
-from producer import Producer
-from consumer import Consumer
-from node import Node
-from dag import Graph
-from utils.debug import stop_if
+from anacostia.streams.directory import DirectoryStream
+from anacostia.consumer import Consumer
+from anacostia.producer import Producer
+from anacostia.transports.local import FileSystemTransport
+from anacostia.node import Node
+from anacostia.dag import Graph
+from anacostia.utils.debug import stop_if
 
+sql = str   # alias of the str type for syntax highlighting using the Python Inline Source Syntax Highlighting extension by Sam Willis in VSCode.
 
-tests_path = "./testing_artifacts"
-db_folder_path = f"{tests_path}/.anacostia"
-input_path1 = f"{tests_path}/incoming1"
-input_path2 = f"{tests_path}/incoming2"
-output_path1 = f"{tests_path}/processed1"
-output_path2 = f"{tests_path}/processed2"
-output_combined_path = f"{tests_path}/processed_combined"
+print("imports successful, starting test...")
+
+tests_path = Path("./testing_artifacts")
+db_folder_path = tests_path / ".anacostia"
+input_path1 = tests_path / "incoming1"
+input_path2 = tests_path / "incoming2"
+output_path1 = tests_path / "processed1"
+output_path2 = tests_path / "processed2"
+output_combined_path = tests_path / "processed_combined"
+transport_package_dir = tests_path / "transport_dir"
+pipeline2_receiver = tests_path / "transport_receiver"
 
 parser = argparse.ArgumentParser(description="Run the pipeline after restart test")
 parser.add_argument("-r", "--restart", action="store_true", help="Flag to indicate if this is a restart")
 args = parser.parse_args()
 
 if args.restart == False:
-    if os.path.exists(tests_path) is True:
+    if tests_path.exists() is True:
         shutil.rmtree(tests_path)
-    os.makedirs(tests_path)
+    tests_path.mkdir(parents=True, exist_ok=True)
 
-log_path = f"{tests_path}/anacostia.log"
+log_path = tests_path / "anacostia.log"
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
-    filename=log_path,
+    filename=str(log_path),
     filemode='a'
 )
 logger = logging.getLogger(__name__)
@@ -59,29 +66,54 @@ stream_consumer_even = Consumer(
 )
 odd_producer = Producer(name="odd_producer", directory=output_path1, logger=logger)   # example producer
 even_producer = Producer(name="even_producer", directory=output_path2, logger=logger)   # example producer
-combined_producer = Producer(name="combined_producer", directory=output_combined_path, logger=logger)   # example producer to write combined results
+combined_producer = Producer(name="combined_producer", directory=output_combined_path, logger=logger)   # example producer
 
-node = Node(name="TestNode", consumers=[stream_consumer_odd, stream_consumer_even], producers=[odd_producer, even_producer, combined_producer], logger=logger)
+combined_transport = FileSystemTransport(name="combined_transport", packages_directory=transport_package_dir, logger=logger)
 
-@node.restart
-def restart_logic():
-    logger.info(f"Node {node.name} executing custom restart logic for run {node.run_id}")
+node = Node(
+    name="TestNode", 
+    consumers=[stream_consumer_odd, stream_consumer_even], 
+    producers=[odd_producer, even_producer, combined_producer], 
+    transports=[combined_transport],
+    logger=logger
+)
 
 @node.entrypoint
 def node_func():
-    odd_staging_path = odd_producer.get_staging_directory()
-    even_staging_path = even_producer.get_staging_directory()
-    combined_staging_path = combined_producer.get_staging_directory()
+    if node.run_id == 0:
+        # if it's the first run (i.e. if it's the first time this pipeline has ever been ran), execute the normal entrypoint logic
+        logger.info(f"Node {node.name} starting entrypoint function for run {node.run_id}")
+    else:
+        # if it's a restart, execute custom logic before resuming the entrypoint function
+        logger.info(f"Node {node.name} executing custom restart logic for run {node.run_id}")
+
+    # All code that comes before this for loop will be executed only on pipeline start and restart, but not on subsequent runs. 
 
     for bundle1, bundle2 in zip(stream_consumer_odd, stream_consumer_even):
+        
+        # All code here will execute prior to the run starting
+        
         with node.stage_run():
+
+            # All code here will execute during the run
+
+            odd_path = odd_producer.get_staging_directory() / f"processed_odd_{node.run_id}.txt"
+            even_path = even_producer.get_staging_directory() / f"processed_even_{node.run_id}.txt"
+            
+            subdir = combined_producer.get_staging_directory() / f"combined_dir_{node.run_id}"
+            os.makedirs(subdir, exist_ok=True)
+            combined_staging_path = subdir / f"processed_combined_{node.run_id}.txt"
+
             for i, (item1, item2) in enumerate(zip(bundle1, bundle2)):
-                with open(os.path.join(odd_staging_path, f"processed_odd_{node.run_id}.txt"), "a") as file:
+
+                # All code here will execute on each item in the bundle
+
+                with open(odd_path, "a") as file:
                     file.write(f"Processed {item1} from odd stream\n")
                 
-                time.sleep(1)   # checkpoint 1
+                #time.sleep(1)   # checkpoint 1
                 
-                with open(os.path.join(even_staging_path, f"processed_even_{node.run_id}.txt"), "a") as file:
+                with open(even_path, "a") as file:
                     file.write(f"Processed {item2} from even stream\n")
                 
                 time.sleep(1)   # checkpoint 2
@@ -91,10 +123,50 @@ def node_func():
                 if args.restart == False:
                     stop_if(current_run=node.run_id, current_iter=i, target_run=1, target_iter=0, mode="sigint", logger=logger) 
                 
-                with open(os.path.join(combined_staging_path, f"processed_combined_{node.run_id}.txt"), "a") as file:
+                with open(combined_staging_path, "a") as file:
                     file.write(f"Processed {item1} and {item2} from combined streams\n")
                 
-                time.sleep(1)   # checkpoint 3
+                #time.sleep(1)   # checkpoint 3
+            
+            # All code here will execute before the run ends but after you are done using the bundle
+
+            odd_producer.commit_artifact(
+                artifact_staging_path=odd_path, 
+                artifact_final_path=odd_producer.get_final_directory() / f"processed_odd_{node.run_id}.txt"
+            )
+
+            even_producer.commit_artifact(
+                artifact_staging_path=even_path, 
+                artifact_final_path=even_producer.get_final_directory() / f"processed_even_{node.run_id}.txt"
+            )
+
+            # commit artifacts you want to keep track of. uncommited artifacts will be deleted when run ends. 
+            combined_file_path, combined_file_hash = combined_producer.commit_artifact(
+                artifact_staging_path=combined_staging_path, 
+                artifact_final_path=combined_producer.get_final_directory() / f"processed_combined_{node.run_id}.txt"
+            )
+
+            # stage artifact for prepare for transport packaging
+            combined_transport.stage_artifact(
+                artifact_path=combined_file_path, 
+                artifact_staging_path=combined_transport.get_staging_directory() / f"processed_combined_{node.run_id}.txt",
+                artifact_hash=combined_file_hash,
+                producer_name=combined_producer.name
+            )
+
+            # package the artifact to create transport package
+            package_path, package_hash = combined_transport.package()
+
+            # optional: use the transport to send the artifact to another pipeline
+            # you can also skip this step if you are manually transporting artifact over air gap.
+            combined_transport.send(
+                package_path=package_path,
+                package_hash=package_hash,
+                dest_directory=pipeline2_receiver
+            )
+        
+        # All code here outside the node.stage_run() context manager will execute after the run ends but before the next run begins
+
 
 graph = Graph(name="TestGraph", nodes=[node], db_folder=db_folder_path, logger=logger)
 
