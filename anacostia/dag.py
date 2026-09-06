1dc20a4c65794f56eb7ad7ac256cbd05c08c21b3
Minh Quan Do <mdo6180@gmail.com>
2026-09-06 03:13:30 -0400

    started merging receiving and storage directories into dag

--- anacostia/dag.py
+++ anacostia/dag.py
@@ -1,3 +1,5 @@
+import threading
+import time
 from typing import List
 from logging import Logger
 from pathlib import Path
@@ -16,6 +18,7 @@ class Graph:
         self.nodes = nodes
         self.db_folder = db_folder
         self.logger = logger
+        self._stop = threading.Event()
 
         if not self.db_folder.exists():
             self.db_folder.mkdir(parents=True, exist_ok=True)
@@ -23,6 +26,16 @@ class Graph:
         db_path = self.db_folder / 'anacostia.db'
         if db_path.exists() is True:
             log(f"Database found at {db_path}. Connecting...", level="info", logger=self.logger)
+
+        self.receiving_directory = self.db_folder / 'receiving'
+        if not self.receiving_directory.exists():
+            self.receiving_directory.mkdir(parents=True, exist_ok=True)
+            log(f"Created receiving directory at {self.receiving_directory}.", level="info", logger=self.logger)
+
+        self.storage_directory = self.db_folder / 'storage'
+        if not self.storage_directory.exists():
+            self.storage_directory.mkdir(parents=True, exist_ok=True)
+            log(f"Created storage directory at {self.storage_directory}.", level="info", logger=self.logger)
 
         self.conn_manager = ConnectionManager(db_path, logger=self.logger)
         self.conn_manager.create_global_tables()
@@ -51,16 +64,27 @@ class Graph:
                 transport.initialize_db_connection(db_path)
                 transport.setup()
 
+    def monitor_receiving_directory(self):
+        log(f"Monitoring receiving directory at {self.receiving_directory}.", level="info", logger=self.logger)
+        while self._stop.is_set() is False:
+            time.sleep(0.5)
+        log(f"Stopped monitoring receiving directory at {self.receiving_directory}.", level="info", logger=self.logger)
+
     def start(self):
         log(f"Starting graph '{self.name}' with {len(self.nodes)} nodes.", level="info", logger=self.logger)
         for node in self.nodes:
             node.start()
+
+        self.monitor_thread = threading.Thread(target=self.monitor_receiving_directory, daemon=True)
+        self.monitor_thread.start()
     
     def join(self):
         for node in self.nodes:
             node.join()
+        self.monitor_thread.join()
     
     def stop(self):
         log(f"Stopping graph '{self.name}' with {len(self.nodes)} nodes.", level="info", logger=self.logger)
+        self._stop.set()
         for node in self.nodes:
             node.stop_consumers()
