28294867eb3f7719973ae30c4362f40818df50f1
Minh Quan Do <mdo6180@gmail.com>
2026-09-14 18:25:50 -0400

    added simple pipeline code for large files

---
+++ single_graph_tests/large_files/trigger_pipeline.py
@@ -0,0 +1,19 @@
+import time
+
+
+def create_file(file_path, content):
+    try:
+        with open(file_path, 'w') as file:
+            file.write(content)
+        print(f"File '{file_path}' created successfully.")
+    except Exception as e:
+        print(f"Error creating the file: {e}")
+
+
+if __name__ == "__main__":
+    tests_path = f"./testing_artifacts"
+    data_store1_input = f"{tests_path}/incoming1"
+
+    for i in range(12):
+        create_file(f"{data_store1_input}/test_file{i}.txt", f"incoming1 {i}")
+        time.sleep(1.5)
